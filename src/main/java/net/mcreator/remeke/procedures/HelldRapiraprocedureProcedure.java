6d8ee7173da2ef3abbec0bac380426fe56b4f91e
EX0FY <sitni@DESKTOP-IS1OAEH>
2021-03-25 15:35:53 +0500

    Helldrapiraprocedure2

--- src/main/java/net/mcreator/remeke/procedures/HelldRapiraprocedureProcedure.java
+++ src/main/java/net/mcreator/remeke/procedures/HelldRapiraprocedureProcedure.java
@@ -1,9 +1,17 @@
 package net.mcreator.remeke.procedures;
 
 import net.minecraft.util.DamageSource;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.horse.ZombieHorseEntity;
+import net.minecraft.entity.passive.horse.SkeletonHorseEntity;
+import net.minecraft.entity.monster.ZombieVillagerEntity;
+import net.minecraft.entity.monster.ZombieEntity;
+import net.minecraft.entity.monster.SpiderEntity;
+import net.minecraft.entity.monster.SkeletonEntity;
+import net.minecraft.entity.monster.EndermiteEntity;
+import net.minecraft.entity.monster.EndermanEntity;
+import net.minecraft.entity.monster.CreeperEntity;
+import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.CreatureAttribute;
 
 import net.mcreator.remeke.RemekeModElements;
 
@@ -22,7 +30,43 @@ public class HelldRapiraprocedureProcedure extends RemekeModElements.ModElement 
 			return;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
-		if ((entity instanceof LivingEntity ? (((LivingEntity) entity).getCreatureAttribute() == CreatureAttribute.UNDEAD) : false)) {
+		if ((entity instanceof CreeperEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof ZombieHorseEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof ZombieVillagerEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof ZombieEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof SpiderEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof SkeletonEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof SkeletonHorseEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof EndermanEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof EndermiteEntity)) {
+			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
+			entity.setFire((int) 8);
+		}
+		if ((entity instanceof EnderDragonEntity)) {
 			entity.attackEntityFrom(DamageSource.GENERIC, (float) 3);
 			entity.setFire((int) 8);
 		}
